a5fdbf99b5cc1b34cb8f7fcfc06264f618dbeb96
skitoo <alexis@skitoo.net>
2015-04-06 10:48:41 +0200

    make dev environment with docker

--- chac/chac/settings.py
+++ chac/chac/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'sensors',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'chac.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('CHAC_DB_NAME', 'chac'),
+        'USER': os.getenv('CHAC_DB_USER', 'chac'),
+        'HOST': os.getenv('CHAC_DB_HOST', 'localhost'),
+        'PORT': os.getenv('CHAC_DB_PORT', 5432)
     }
 }
 
