89163592ef8a00d581c43afa00679c902481560d
skitoo <alexis@skitoo.net>
2015-04-06 11:00:42 +0200

    add Sensor model

--- chac/sensors/models.py
+++ chac/sensors/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from jsonfield import JSONField
 
-# Create your models here.
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=25)
+    activated = models.BooleanField(default=False)
+    type = models.CharField(max_length=10)
+    meta = JSONField()
